0e5c618cd320d25df5aea30773cf79036af27778
Nonwarit <nonwarit9636@gmail.com>
2023-09-06 20:31:41 +0700

    first commit

---
+++ Anniversary.js
@@ -0,0 +1,18 @@
+function passcheck() {
+    const result = document.getElementById("pass1").value;
+    let passcode = 20230906;
+    let space = '';
+
+if (result == space) {
+       alert("Enter password jaaa")
+} 
+    else {
+       if (result == passcode) {
+          window.open("https://chlruxpznynflnfwyuxe9q.on.drv.tw/www.PoBell.blog/index.html.html");
+} 
+    else {
+          alert("Incorrect Password jaa (Hint: Our 5th years Anniversary date)");
+          location.reload();
+}
+    }
+}
